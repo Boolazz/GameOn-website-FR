b2be5074ca1a60b8510a86bfe9be70401591368f
Lucas B <94980335+Boolazz@users.noreply.github.com>
2022-05-03 13:07:39 +0200

    closemodalform

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalCloseBtn = document.querySelectorAll(".close");
+const form = document.getElementsByName('reserve');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +21,26 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+// Close modal event
+modalCloseBtn[0].addEventListener ("click", closeModal);
+
+// Keep form data
+form[0].addEventListener('submit', (e) => {
+  e.preventDefault();
+});
+
+// Check validation of condition provided
+function checkCondition(condition){
+  if(!condition) return false;
+  else return true;
+}
+
+
 
 
